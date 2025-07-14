470cd034d487fb61315c4f720101a520079f59d4
Sumith U <sumithsumith4567890@gmail.com>
2025-07-14 21:21:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getInitials(name: string) {
+  if (!name) return '';
+  const names = name.split(' ');
+  const initials = names.map(n => n[0]).join('').toUpperCase();
+  if (initials.length > 2 && names.length > 1) {
+    return `${initials[0]}${initials[initials.length - 1]}`;
+  }
+  return initials.substring(0, 2);
+}
