e00854754d2f899eaeadc197bb2530468b15d92d
Sumith U <sumithsumith4567890@gmail.com>
2025-07-14 21:27:35 +0000

    i need slow transition

--- src/components/dashboard/swipe-deck.tsx
+++ src/components/dashboard/swipe-deck.tsx
@@ -47,7 +47,7 @@ function AnimatedCard({
         x: dragEndVelocity > 0 ? 500 : -500,
         opacity: 0,
         scale: 0.9,
-        transition: { duration: 0.3 } 
+        transition: { duration: 0.5 } 
       }}
       initial={{ scale: 0.95, opacity: 0 }}
       animate={{ scale: 1, opacity: 1, transition: { duration: 0.3 } }}
