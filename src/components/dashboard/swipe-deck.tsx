4c33043255a0325e24f7e024930c83effe0a7bb5
Sumith U <sumithsumith4567890@gmail.com>
2025-07-16 11:15:45 +0000

    make more transparent

--- src/components/dashboard/swipe-deck.tsx
+++ src/components/dashboard/swipe-deck.tsx
@@ -344,7 +344,7 @@ export function SwipeDeck({ users: initialUsers, currentUserId }: SwipeDeckProps
       </div>
       {activeUser && (
         <AlertDialog open={isBlockConfirmOpen} onOpenChange={setIsBlockConfirmOpen}>
-            <AlertDialogContent className="bg-black border-neutral-800">
+            <AlertDialogContent className="bg-black/70 backdrop-blur-md border-neutral-800">
                 <AlertDialogHeader>
                 <AlertDialogTitle>Block {activeUser.name}?</AlertDialogTitle>
                 <AlertDialogDescription>
