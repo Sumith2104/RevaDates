57db76006cac58b15e0aa4b1d265a9830130a361
Sumith U <sumithsumith4567890@gmail.com>
2025-07-17 20:04:18 +0000

    i need thick border for text box in chat

--- src/app/chats/[chatId]/page.tsx
+++ src/app/chats/[chatId]/page.tsx
@@ -205,7 +205,7 @@ export default function ChatPage() {
                         value={newMessage}
                         onChange={(e) => setNewMessage(e.target.value)}
                         placeholder="Type a message..."
-                        className="flex-1 border"
+                        className="flex-1 border-2"
                         autoComplete="off"
                         disabled={sending}
                     />
