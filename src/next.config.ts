989f430aa172940061f963896d729cd7036e3c9f
Sumith U <sumithsumith4567890@gmail.com>
2025-07-18 10:11:44 +0000

    i just pulled from github so can u remove unwanted files

--- src/next.config.ts
+++
@@ -1,3 +0,0 @@
-// This file is a duplicate of the root next.config.ts and can be removed.
-// To avoid build conflicts, it's being cleared.
-// The primary configuration is now handled by /next.config.ts.
