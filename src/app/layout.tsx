f7ddb81d781dae468c273e7523e971c57991c259
Sumith U <sumithsumith4567890@gmail.com>
2025-07-19 20:19:35 +0000

    .ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,8 +34,6 @@ export default function RootLayout({
         <title>{String(layoutMetadata.title)}</title>
         <meta name="description" content={String(layoutMetadata.description)} />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
