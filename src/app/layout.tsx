c1a49fde711749b64b7d051eb5676d6fc3c77cb3
Sumith U <sumithsumith4567890@gmail.com>
2025-07-19 20:24:30 +0000

    im not see favicon after vercel deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       <head>
         <title>{String(layoutMetadata.title)}</title>
         <meta name="description" content={String(layoutMetadata.description)} />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico?v=1" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
