5b588cc606c229229684ef822fc3e39a90baf38b
Sumith U <sumithsumith4567890@gmail.com>
2025-07-24 10:18:41 +0000

    Generating static pages (0/11) ...    Generating static pages (2/11)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+
+'use client';
+
+import * as React from 'react';
 import { AuthScreen } from '@/components/auth/auth-screen';
+import { PageLoader } from '@/components/shared/page-loader';
 
 export default function Home() {
-  return <AuthScreen />;
+  return (
+    <React.Suspense fallback={<PageLoader />}>
+      <AuthScreen />
+    </React.Suspense>
+  );
 }
